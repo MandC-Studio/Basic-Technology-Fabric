c59b54ad4b1af88792485638c67dbdc1dd66552f
Dorion Y. Chou <Joy.YiZhou@proton.me>
2024-06-09 00:05:25 +0800

    Client入口

---
+++ src/main/java/cn/org/mandcstudio/basictechnology/BasicTechnologyClient.java
@@ -0,0 +1,10 @@
+package cn.org.mandcstudio.basictechnology;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicTechnologyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
