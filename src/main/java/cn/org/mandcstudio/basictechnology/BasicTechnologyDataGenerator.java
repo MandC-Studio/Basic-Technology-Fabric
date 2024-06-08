72db1954cb8f4682d8e1b2a9b16587509d9d0ae5
Dorion Y. Chou <Joy.YiZhou@proton.me>
2024-06-08 23:09:28 +0800

    1

---
+++ src/main/java/cn/org/mandcstudio/basictechnology/BasicTechnologyDataGenerator.java
@@ -0,0 +1,11 @@
+package cn.org.mandcstudio.basictechnology;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BasicTechnologyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
