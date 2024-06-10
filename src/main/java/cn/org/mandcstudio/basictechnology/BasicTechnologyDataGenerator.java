be6cbf29ea3051f349de644443dc420004c6b546
Dorion Y. Chou <Joy.YiZhou@proton.me>
2024-06-10 11:40:08 +0800

    datagen

--- src/main/java/cn/org/mandcstudio/basictechnology/BasicTechnologyDataGenerator.java
+++ src/main/java/cn/org/mandcstudio/basictechnology/BasicTechnologyDataGenerator.java
@@ -1,11 +1,18 @@
 package cn.org.mandcstudio.basictechnology;
 
+import cn.org.mandcstudio.basictechnology.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class BasicTechnologyDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
